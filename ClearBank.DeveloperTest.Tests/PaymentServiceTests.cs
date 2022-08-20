56b0f2d05408929308940cda5435f03dcb67f019
Craig Shepherd <craig.w.j.shepherd@outlook.com>
2022-08-20 10:42:31 +0100

    Changed name of unit tests file to reflect purpose.

---
+++ ClearBank.DeveloperTest.Tests/PaymentServiceTests.cs
@@ -0,0 +1,27 @@
+using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using System;
+using Xunit;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+	public class PaymentServiceTests
+	{
+		[Fact]
+		public void MakePayment_GivenValidParameter_ReturnsSuccessfulResult()
+		{
+			// Arrange
+			var debtorAccount = new Account() { AccountNumber = "0123456789", AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments, Balance = 100m, Status = AccountStatus.Live };
+			var paymentRequest = new MakePaymentRequest() { Amount = 10m, CreditorAccountNumber = "9876543210", DebtorAccountNumber = "0123456789", PaymentDate = DateTime.Now, PaymentScheme = PaymentScheme.FasterPayments };
+
+			var paymentService = new PaymentService(debtorAccount);
+
+			// Act
+			MakePaymentResult result = paymentService.MakePayment(paymentRequest);
+
+			// Assert
+			Assert.NotNull(result);
+			Assert.True(result.Success);
+		}
+	}
+}
