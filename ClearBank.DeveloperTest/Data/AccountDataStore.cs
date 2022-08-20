bfff7b761b0de09aadc43665dcb562e8eac0deab
Craig Shepherd <craig.w.j.shepherd@outlook.com>
2022-08-20 10:36:44 +0100

    Added hard-coded Account object for testing.

--- ClearBank.DeveloperTest/Data/AccountDataStore.cs
+++ ClearBank.DeveloperTest/Data/AccountDataStore.cs
@@ -2,17 +2,28 @@
 
 namespace ClearBank.DeveloperTest.Data
 {
-    public class AccountDataStore
-    {
-        public Account GetAccount(string accountNumber)
-        {
-            // Access database to retrieve account, code removed for brevity 
-            return new Account();
-        }
+	public class AccountDataStore
+	{
+		private readonly Account _testDebtorAccount = new Account() { AccountNumber = "0123456789", AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments, Balance = 100m, Status = AccountStatus.Live };
 
-        public void UpdateAccount(Account account)
-        {
-            // Update account in database, code removed for brevity
-        }
-    }
+		public Account GetAccount(string accountNumber)
+		{
+			// Access database to retrieve account, code removed for brevity
+			// Added hard-coded Account object for testing
+			if (accountNumber == "0123456789")
+			{
+				return _testDebtorAccount;
+			}
+			else
+			{
+				return new Account();
+			}
+		}
+
+		// Changed to return boolean value to give indication of success or failure of I/O method
+		public void UpdateAccount(Account account)
+		{
+			// Update account in database, code removed for brevity
+		}
+	}
 }
