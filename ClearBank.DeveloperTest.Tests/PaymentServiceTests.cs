6fd60c9ada9fb6f126f5bcd35dbc68959a607cdc
Craig Shepherd <craig.w.j.shepherd@outlook.com>
2022-08-20 10:48:12 +0100

    Removed debtor account from test as no longer needed.

--- ClearBank.DeveloperTest.Tests/PaymentServiceTests.cs
+++ ClearBank.DeveloperTest.Tests/PaymentServiceTests.cs
@@ -11,10 +11,8 @@ namespace ClearBank.DeveloperTest.Tests
 		public void MakePayment_GivenValidParameter_ReturnsSuccessfulResult()
 		{
 			// Arrange
-			var debtorAccount = new Account() { AccountNumber = "0123456789", AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments, Balance = 100m, Status = AccountStatus.Live };
-			var paymentRequest = new MakePaymentRequest() { Amount = 10m, CreditorAccountNumber = "9876543210", DebtorAccountNumber = "0123456789", PaymentDate = DateTime.Now, PaymentScheme = PaymentScheme.FasterPayments };
-
 			var paymentService = new PaymentService();
+			var paymentRequest = new MakePaymentRequest() { Amount = 10m, CreditorAccountNumber = "9876543210", DebtorAccountNumber = "0123456789", PaymentDate = DateTime.Now, PaymentScheme = PaymentScheme.FasterPayments };
 
 			// Act
 			MakePaymentResult result = paymentService.MakePayment(paymentRequest);
