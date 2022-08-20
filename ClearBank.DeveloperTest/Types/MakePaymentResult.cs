33d75d809bc201e23fa9fd6d17d450433eaf7e69
Craig Shepherd <craig.w.j.shepherd@outlook.com>
2022-08-20 10:32:25 +0100

    Changed MakePaymentResult to default to successful state to fix PaymentService.

--- ClearBank.DeveloperTest/Types/MakePaymentResult.cs
+++ ClearBank.DeveloperTest/Types/MakePaymentResult.cs
@@ -1,7 +1,8 @@
 ﻿namespace ClearBank.DeveloperTest.Types
 {
-    public class MakePaymentResult
-    {
-        public bool Success { get; set; }
-    }
+	public class MakePaymentResult
+	{
+		// Made to instantiate to true in order to fix MakePayment method
+		public bool Success { get; set; } = true;
+	}
 }
