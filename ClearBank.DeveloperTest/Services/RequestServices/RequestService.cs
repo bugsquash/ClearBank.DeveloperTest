282f6e6ad62d8c8af36cfac86c7bf34039641d29
Craig Shepherd <craig.w.j.shepherd@outlook.com>
2022-08-24 17:24:37 +0100

    Simplified request service.

--- ClearBank.DeveloperTest/Services/RequestServices/RequestService.cs
+++ ClearBank.DeveloperTest/Services/RequestServices/RequestService.cs
@@ -3,45 +3,37 @@ using ClearBank.DeveloperTest.Types;
 
 namespace ClearBank.DeveloperTest.Services.RequestServices
 {
-    public class RequestService : IRequestService
-    {
-        public MakePaymentResult ValidatRequestWithAccount(MakePaymentRequest request, Account account)
-        {
-            var result = new MakePaymentResult() { Success = true };
+	public class RequestService : IRequestService
+	{
+		public MakePaymentResult ValidatRequestWithAccount(MakePaymentRequest request, Account account)
+		{
+			var result = new MakePaymentResult() { Success = true };
 
-            switch (request.PaymentScheme)
-            {
-                case PaymentScheme.Bacs:
-                    if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Bacs))
-                    {
-                        result.Success = false;
-                    }
-                    break;
+			if (request.PaymentScheme.ToString() != account.AllowedPaymentSchemes.ToString())
+			{
+				result.Success = false;
+			}
+			else
+			{
+				switch (request.PaymentScheme)
+				{
+					case PaymentScheme.FasterPayments:
+						if (account.Balance < request.Amount)
+						{
+							result.Success = false;
+						}
+						break;
 
-                case PaymentScheme.FasterPayments:
-                    if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.FasterPayments))
-                    {
-                        result.Success = false;
-                    }
-                    else if (account.Balance < request.Amount)
-                    {
-                        result.Success = false;
-                    }
-                    break;
+					case PaymentScheme.Chaps:
+						if (account.Status != AccountStatus.Live)
+						{
+							result.Success = false;
+						}
+						break;
+				}
+			}
 
-                case PaymentScheme.Chaps:
-                    if (!account.AllowedPaymentSchemes.HasFlag(AllowedPaymentSchemes.Chaps))
-                    {
-                        result.Success = false;
-                    }
-                    else if (account.Status != AccountStatus.Live)
-                    {
-                        result.Success = false;
-                    }
-                    break;
-            }
-
-            return result;
-        }
-    }
+			return result;
+		}
+	}
 }
