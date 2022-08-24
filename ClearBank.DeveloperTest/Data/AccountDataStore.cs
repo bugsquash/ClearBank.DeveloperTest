e6e4fba490c1e9d115fdfa24529c6b84e21da262
Craig Shepherd <craig.w.j.shepherd@outlook.com>
2022-08-24 17:17:53 +0100

    Added interface for Account Data Stores and removed hard-coding for testing.

--- ClearBank.DeveloperTest/Data/AccountDataStore.cs
+++ ClearBank.DeveloperTest/Data/AccountDataStore.cs
@@ -2,25 +2,14 @@
 
 namespace ClearBank.DeveloperTest.Data
 {
-	public class AccountDataStore
+	public class AccountDataStore : IAccountDataStore
 	{
-		private readonly Account _testDebtorAccount = new Account() { AccountNumber = "0123456789", AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments, Balance = 100m, Status = AccountStatus.Live };
-
 		public Account GetAccount(string accountNumber)
 		{
-			// Access database to retrieve account, code removed for brevity
-			// Added hard-coded Account object for testing
-			if (accountNumber == "0123456789")
-			{
-				return _testDebtorAccount;
-			}
-			else
-			{
-				return new Account();
-			}
+			// Access database to retrieve account, code removed for brevity 
+			return new Account();
 		}
 
-		// Changed to return boolean value to give indication of success or failure of I/O method
 		public void UpdateAccount(Account account)
 		{
 			// Update account in database, code removed for brevity
