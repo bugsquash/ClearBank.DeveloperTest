ac8f12c40a873657fe8539d94b36ec3e05a36067
Craig Shepherd <craig.w.j.shepherd@outlook.com>
2022-08-24 09:21:46 +0100

    Added moq and used to mock services for better unit tests.

--- ClearBank.DeveloperTest.Tests/PaymentServiceTests.cs
+++ ClearBank.DeveloperTest.Tests/PaymentServiceTests.cs
@@ -1,5 +1,6 @@
 using ClearBank.DeveloperTest.Services;
 using ClearBank.DeveloperTest.Types;
+using Moq;
 using System;
 using Xunit;
 
@@ -11,7 +12,15 @@ namespace ClearBank.DeveloperTest.Tests
 		public void MakePayment_GivenValidParameter_ReturnsSuccessfulResult()
 		{
 			// Arrange
-			var paymentService = new PaymentService();
+			var accountServiceMock = new Mock<IAccountService>();
+			accountServiceMock.Setup(x => x.GetAccount(It.IsAny<string>(), It.IsAny<string>())).Returns(new Account() { AccountNumber = "0123456789", AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments, Balance = 100m, Status = AccountStatus.Live });
+
+			var requestServiceMock = new Mock<IRequestService>();
+			requestServiceMock.Setup(x => x.ValidatRequestWithAccount(It.IsAny<MakePaymentRequest>(), It.IsAny<Account>())).Returns(new MakePaymentResult() { Success = true });
+
+			var databaseServiceMock = new Mock<IDatabaseService>();
+
+			var paymentService = new PaymentService(accountServiceMock.Object, requestServiceMock.Object, databaseServiceMock.Object);
 			var paymentRequest = new MakePaymentRequest() { Amount = 10m, CreditorAccountNumber = "9876543210", DebtorAccountNumber = "0123456789", PaymentDate = DateTime.Now, PaymentScheme = PaymentScheme.FasterPayments };
 
 			// Act
